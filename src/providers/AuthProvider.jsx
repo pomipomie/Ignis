f94a60f93c7f28c2c6ac94525b2ba1a9fb025c5f
Xio <mxbadano@gmail.com>
2023-10-08 13:54:20 -0300

    comment

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -22,6 +22,7 @@ export function AuthProvider({children}){
   )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useAuth(){
   const context = useContext(AuthContext);
   return context;
